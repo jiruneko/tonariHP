2c33262365a2314c0c402fbc2a18122da1201ea1
jiruneko <oohikaru310@gmail.com>
2026-05-09 22:59:30 +0900

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,15 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get("authorization");
+  const basicAuth =
+    req.headers.get("authorization");
 
   if (basicAuth) {
-    const authValue = basicAuth.split(" ")[1];
+    const authValue =
+      basicAuth.split(" ")[1];
 
-    const [user, password] = atob(authValue).split(":");
+    const [user, password] =
+      atob(authValue).split(":");
 
     if (
       user === process.env.BASIC_AUTH_USER &&
